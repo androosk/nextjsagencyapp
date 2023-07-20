94c6f36b16c098eb370b6f8bd01b4d6b997df145
androosk <atirpok3@gmail.com>
2023-07-20 13:13:10 -0600

    more ui

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+"use client"
+import React, { useEffect } from 'react';
 import { manrope } from '../fonts';
 
-const withManrope = `${manrope.className} text-2xl md:text-6xl font-bold tracking-wider`
+const withManropeLeft = `${manrope.className} text-2xl md:text-6xl font-bold tracking-wider relative transform transition duration-500 -translate-x-[120%] -translate-y-10`
+
+const withManropeRight = `${manrope.className} text-2xl md:text-6xl font-bold tracking-wider relative transform transition duration-500 translate-x-[120%] translate-y-10`
 
 const Hero = () => {
+
+const loadFunction = () => {
+      const replacers = document.querySelectorAll('[data-replace]');
+      for(let i=0; i<replacers.length; i++){
+          let replaceClasses = JSON.parse(replacers[i].dataset.replace.replace(/'/g, '"'));
+          Object.keys(replaceClasses).forEach(function(key) {
+              replacers[i].classList.remove(key);
+              replacers[i].classList.add(replaceClasses[key]);
+          });
+      }
+  };
+
+useEffect(() => {
+  loadFunction()
+}, [])
+
   return (
     <section className='bg-white text-black h-52 md:h-[450px] flex items-start place-content-center flex-col px-[10%]'>
-       <h1 className={withManrope}>Beautiful websites that</h1>
-       <p className={withManrope}>bring customers to your door</p>
+       <h1 className={withManropeLeft} data-replace='{"-translate-x-[120%]": "translate-x-0", "-translate-y-10": "translate-y-0"}'>Beautiful websites that</h1>
+       <p className={withManropeRight} data-replace='{"translate-x-[120%]": "translate-x-0", "translate-y-10": "translate-y-0"}'>bring customers to your door</p>
     </section>
   )
 }
--- app/components/InfoBar.jsx
+++ app/components/InfoBar.jsx
@@ -3,9 +3,28 @@ import React from 'react'
 const InfoBar = () => {
   return (
     <section className='bg-white h-52 md:h-[450px] flex items-start place-content-center flex-col'>
-      <div className='relative flex overflow-x-hidden'>
-        <div className='bg-black w-[100vw] py-2 animate-marquee whitespace-nowrap'>
-          <span className='mx-4 text-2xl'>text here</span>
+      <div className='relative flex overflow-x-hidden bg-black'>
+        <div className='py-2 animate-marquee whitespace-nowrap'>
+          <span className='mx-4 text-2xl'>text here1</span>
+          <span className='mx-4 text-2xl'>text here2</span>
+          <span className='mx-4 text-2xl'>text here3</span>
+          <span className='mx-4 text-2xl'>text here4</span>
+          <span className='mx-4 text-2xl'>text here5</span>
+          <span className='mx-4 text-2xl'>text here6</span>
+          <span className='mx-4 text-2xl'>text here7</span>
+          <span className='mx-4 text-2xl'>text here8</span>
+          <span className='mx-4 text-2xl'>text here9</span>
+        </div>
+        <div className=' absolute top-0 py-2 animate-marquee2 whitespace-nowrap'>
+          <span className='mx-4 text-2xl'>text here10</span>
+          <span className='mx-4 text-2xl'>text here11</span>
+          <span className='mx-4 text-2xl'>text here12</span>
+          <span className='mx-4 text-2xl'>text here13</span>
+          <span className='mx-4 text-2xl'>text here14</span>
+          <span className='mx-4 text-2xl'>text here15</span>
+          <span className='mx-4 text-2xl'>text here16</span>
+          <span className='mx-4 text-2xl'>text here17</span>
+          <span className='mx-4 text-2xl'>text here18</span>
         </div>
 
       </div>
